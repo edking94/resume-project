b87acae789d959484aebce581468bf0b32d7e7ea
edking94 <kingperformancetherapy@gmail.com>
2020-06-16 13:53:01 +0000

    map coordinates personalised

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,22 +2,31 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 54.5260,
+            lng: 15.2551
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [{
-        lat: 40.785091,
-        lng: -73.968285
+        lat: 53.1424,
+        lng: -7.6921
     }, {
-        lat: 41.084045,
-        lng: -73.874245
+        lat: 55.3781,
+        lng: -3.4360
     }, {
-        lat: 40.754932,
-        lng: -73.984016
+        lat: 46.2276,
+        lng: -2.2137
+    }, {
+        lat: 40.4637,
+        lng: -3.7492
+    }, {
+        lat: 51.1657,
+        lng: 10.4515
+    }, {
+        lat: 41.8719,
+        lng: 12.5674
     }];
 
     var markers = locations.map(function (location, i) {
